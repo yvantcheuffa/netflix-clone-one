9395295ba9b42c6843452620d59a85cf5ae7a97c
tcshack <tcheuffayvan@yahoo.com>
2020-10-03 14:06:29 +0100

    Started working on jumbotron.

--- src/components/jumbotron/styles/Jumbotron.js
+++ src/components/jumbotron/styles/Jumbotron.js
@@ -20,17 +20,32 @@ export const Container = styled.div `
 `;
 
 export const Image = styled.img `
-
+    max-width: 100%;
 `;
 
 export const Title = styled.h1 `
-    background-color: green;
+    font-size: 50px;
+    line-height: 1.1;
+    margin-bottom: 8px;
+    
+    @media(max-width: 600px) {
+        font-size: 35px;
+    }
 `;
 
-export const SubTitle = styled.div `
-    background-color: grey;
+export const SubTitle = styled.h2 `
+    font-size: 26px;
+    font-weight: normal;
+    line-height: normal;
+    
+    @media (max-width: 600px) {
+        font-size: 18px;
+    }
 `;
 
 export const Pane = styled.div `
-
+    width: 50%;
+    @media(max-width: 1000px) {
+        width: 100%;
+    }
 `;
